04b4ed4f0aec73310290ab10638200c2ee0c5f2b
sophiasheref <sophia.sheref@gmail.com>
2017-11-22 23:55:43 -0500

    done

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+  {
+    'name': 'Sophia',
+    'photo': 'https://greenchikin.deviantart.com/art/Cute-Koala-76947536',
+    'scores': [
+      "5",
+      "1",
+      "4",
+      "2",
+      "4",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4"
+    ]
+  },
+  {
+    'name': 'Mike',
+    'photo': 'http://wlpapers.com/cute-dog.html',
+    'scores': [
+      "2",
+      "5",
+      "3",
+      "1",
+      "4",
+      "5",
+      "2",
+      "2",
+      "5",
+      "1"
+    ]
+  },
+  {
+    'name': 'Nancy',
+    'photo': 'http://www.twitrcovers.com/twitter-covers/cute-cat/',
+    'scores': [
+      "3",
+      "1",
+      "2",
+      "1",
+      "2",
+      "5",
+      "3",
+      "4",
+      "5",
+      "2"
+    ]
+  },
+]
+
+
+module.exports = friends;
